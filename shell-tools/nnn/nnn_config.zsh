5f68fac8822e599874fc6ed2dcdcf42b325ff4f4
JohnFrankz <86246881+JohnFrankz@users.noreply.github.com>
2023-10-01 18:52:04 +0800

    nnn use trash-cli

--- shell-tools/nnn/nnn_config.zsh
+++ shell-tools/nnn/nnn_config.zsh
@@ -4,4 +4,5 @@
 NNN_PLUG_DEFAULT='1:ipinfo;p:preview-tui;o:fzz;b:nbak;i:imgview;l:mimelist'
 # NNN_PLUG="$NNN_PLUG_PERSONAL;$NNN_PLUG_WORK;$NNN_PLUG_DEFAULT;$NNN_PLUG_INLINE"
 NNN_PLUG="$NNN_PLUG_DEFAULT"
+export NNN_TRASH=1
 export NNN_PLUG
