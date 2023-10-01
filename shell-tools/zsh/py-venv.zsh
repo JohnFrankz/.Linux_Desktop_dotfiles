379a7473db64c1c3ff306b8d22539c06182693a2
JohnFrankz <86246881+JohnFrankz@users.noreply.github.com>
2023-10-01 19:31:06 +0800

    add python venv alias

---
+++ shell-tools/zsh/py-venv.zsh
@@ -0,0 +1,13 @@
+source /home/rice/Development/python3-venv/default/bin/activate
+
+alias pyvenv-default="source /home/rice/Development/python3-venv/default/bin/activate"
+
+check_venv() {
+    if [[ -n $VIRTUAL_ENV ]]; then
+        echo "Virtual environment is active at: $VIRTUAL_ENV"
+    else
+        echo "No virtual environment is active."
+    fi
+}
+
+alias pyvenv="check_venv"
